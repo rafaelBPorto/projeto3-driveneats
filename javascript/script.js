2048d1205bd4002525b709af486cb5ee1a62c594
Rafael Braga Porto <rafaelbporto88@gmail.com>
2022-08-13 17:50:57 -0300

    botao de pedido ativado

--- javascript/script.js
+++ javascript/script.js
@@ -1,36 +1,62 @@
+//Selecao de items
+let pratoSelecionado = false
+let bebidaSelecionada = false
+let sobremesaSelecionada =false
 function selecionarPrato(prato) {
 
-    const pratoSelecionado = document.querySelector('.prato .item-selecionado');
-
-    console.log(pratoSelecionado);
+    const itemSelecionado = document.querySelector('.prato .item-selecionado');
     
-    prato.classList.add('item-selecionado')
-
-    if (pratoSelecionado !== null) {
-        pratoSelecionado.classList.remove('item-selecionado')
+    
+    if ( itemSelecionado!== null) {
+        itemSelecionado.classList.remove('item-selecionado');
     }
+    prato.classList.add('item-selecionado');
+    pratoSelecionado = true;
     
+    habilitarPedido();
 }
 
 function selecionarBebida(bebida) {
 
-    const bebidaSelecionado = document.querySelector('.bebida .item-selecionado');
+    const itemSelecionado = document.querySelector('.bebida .item-selecionado');
     
-    bebida.classList.toggle('item-selecionado')
     
-    if (bebidaSelecionado !== null) {
-        bebidaSelecionado.classList.remove('item-selecionado')
+    if (itemSelecionado !== null) {
+        itemSelecionado.classList.remove('item-selecionado')
     }
-    
+    bebida.classList.add('item-selecionado');
+    bebidaSelecionada = true;
+    habilitarPedido();
 }
 
 function selecionarSobremesa(item) {
 
     const itemSelecionado = document.querySelector('.sobremesa .item-selecionado');
-    item.classList.add('item-selecionado')
-
+    
     if (itemSelecionado !== null) {
-        itemSelecionado.classList.remove('item-selecionado')
+        itemSelecionado.classList.remove('item-selecionado');
+    }
+    ;
+    item.classList.add('item-selecionado');
+    sobremesaSelecionada = true;
+    habilitarPedido();
+
+}
+
+//Ativacao do botao de envio
+
+function habilitarPedido(){
+    if(pratoSelecionado && bebidaSelecionada && sobremesaSelecionada){
+        console.log("pedido ok");
+
+        //mudar cor do botão
+        const botaoPedido = document.querySelector('.selecao');
+        botaoPedido.classList.add('botao-pedido-ativo');
+
+        //mudar texto do botão
+        const textoBotaoPedido = document.querySelector('.selecao h1');
+        textoBotaoPedido.innerHTML = "Fechar Pedido"
     }
 
 }
+
